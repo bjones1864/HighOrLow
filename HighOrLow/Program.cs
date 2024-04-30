16ae62550c71c4bb53d955e2d5113f2b7f768f03
Ben Jones <benjones1864@gmail.com>
2024-04-30 17:33:07 -0400

    first commit

---
+++ HighOrLow/Program.cs
@@ -0,0 +1,39 @@
+﻿var rand = new Random();
+
+Console.WriteLine("Let's play a guessing game!  Try to guess the number, which will be an integer between 1 and 100.");
+
+do
+{
+    int n = rand.Next(1, 101);
+    int guesses = 0;
+
+    do
+    {
+        Console.WriteLine("Guess a number between 1 and 100:");
+        int guess = int.Parse(Console.ReadLine());
+        
+        if(guess > n)
+        {
+            Console.WriteLine($"{guess} is too high.");
+            guesses++;
+        }
+        else if(guess < n)
+        {
+            Console.WriteLine($"{guess} is too low.");
+            guesses++;
+        }
+        else
+        {
+            Console.WriteLine($"{guess} is correct!  Nice job!");
+            guesses++;
+            break;
+        }
+
+    } while (true);
+
+    Console.WriteLine("Would you like to play again?  Please enter y or yes to play again");
+    string playAgain = Console.ReadLine();
+    
+    if (playAgain.ToLower() != "y" && playAgain.ToLower() != "yes") break;
+
+} while (true);
